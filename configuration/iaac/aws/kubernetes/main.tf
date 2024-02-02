fe73ec458dd5ac040d782b66ff45ca20e5c3020f
Anubharanidharan <156429800+Anubharanidharan@users.noreply.github.com>
2024-02-02 10:38:36 +0530

    main.tf

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -32,7 +32,8 @@ module "in28minutes-cluster" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "in28minutes-cluster"
   cluster_version = "1.14"
-  subnets         = ["subnet-081955365f9382699", "subnet-0eafb27dc0ede03ee"]  #CHANGE
+  #subnets         = ["subnet-081955365f9382699", "subnet-0eafb27dc0ede03ee"]  #CHANGE
+  subnets         = ["subnet-3f7b2563", "subnet-4a7d6a45"]
   #subnets = data.aws_subnet_ids.subnets.ids
   vpc_id          = aws_default_vpc.default.id
 
